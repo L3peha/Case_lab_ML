56d9cad10f4ed285ae6b4c12097a45837835af42
L3peha <merzljakov-alexey@yandex.ru>
2024-10-16 17:15:31 +0300

    start

---
+++ tomatoes/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    film = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField()
+    status = models.BooleanField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
